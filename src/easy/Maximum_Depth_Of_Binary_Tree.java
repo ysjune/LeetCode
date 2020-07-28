be8aa61bcb3a1fae63011cf4e7dd28c348e649b3
ysjune <windsyou@gmail.com>
2020-07-28 23:15:38 +0900

    이진트리 최대 깊이

---
+++ src/easy/Maximum_Depth_Of_Binary_Tree.java
@@ -0,0 +1,62 @@
+package easy;
+
+public class Maximum_Depth_Of_Binary_Tree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     *
+     * Given a binary tree, find its maximum depth.
+     *
+     * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+     *
+     * Note: A leaf is a node with no children.
+     *
+     * Example:
+     * Given binary tree [3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * return its depth = 3
+     *
+     *
+     */
+    public static void main(String[] args) {
+        Maximum_Depth_Of_Binary_Tree solution = new Maximum_Depth_Of_Binary_Tree();
+        solution.maxDepth(new TreeNode(3));
+    }
+}
